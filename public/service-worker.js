5ed9feaa094ccb8c2598136fef3b7dafa9f1b874
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-18 23:27:43 -0700

    added additional cache for backend data

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,18 +2,55 @@
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "v01";
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = APP_PREFIX + "Data-" + VERSION;
 
 const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
     "./js/index.js",
-    "./js/idb.js"
+    "./js/idb.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-512x512.png"
 ];
 
 self.addEventListener("fetch", event => {
     console.log("fetch request: " + event.request.url);
+
+    if (event.request.url.includes("/api/")) { // if api request
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME)
+                .then(cache => {
+                    // fetch data from backend
+                    return fetch(event.request)
+                        // if response is ok, store in cache
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone());
+                            }
+                            return response;
+                        })
+                        // otherwise, attempt to get from cache
+                        .catch(err => cache.match(event.request));
+                })
+                .catch(err => console.log(err))
+        );
+        return;
+    }
+
     event.respondWith(
-        caches.match(event.request).then(request => request || fetch(event.request))
+        fetch(event.request).catch(() => {
+            return caches.match(event.request).then(response => {
+                if (response) {
+                    return response;
+                }
+                else if (event.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index.html");
+                }
+            });
+        })
     );
 });
 
